61b08cbc981fe5a0247f40c5892c473b9fefa1dd
alekscooper <akuptsov-hse-lyceum@yandex.ru>
2026-02-15 11:53:44 +0300

    add prime factors and divisors

---
+++ 25/prime_factors_and_divisors.py
@@ -0,0 +1,27 @@
+def prime_factors(number):
+    """
+    Разложение числа на простые множители
+    """
+    divs = []
+    for _i in range(2, int(number ** 0.5) + 1):
+        while number % _i == 0:
+            divs.append(_i)
+            number //= _i
+    if number > 1:
+        divs.append(number)
+    return divs
+
+
+
+def divisors(n):
+    """
+    Поиск всех делителей
+    """
+    _divisors = []
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            _divisors.append(i)
+            _divisors.append(n // i)
+    if int(n ** 0.5) ** 2 == n:
+        _divisors.pop()
+    return _divisors
